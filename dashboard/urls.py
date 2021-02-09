609d861c01464806c0524761ab4a487ab72f617b
Bhawani singh Shekhawat <finisher@apples-iMac.local>
2021-02-09 17:43:47 +0530

    dashboard pages or we can say wervices

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import home,logout_v
+from .views import home,logout_v,sales_hub,marketing_hub,customer_support
 urlpatterns = [
+	path('sales/',sales_hub,name='sales_url'),
+	path('marketing/',marketing_hub,name='marketing_url'),
+	path('customer/',customer_support,name='customer_url'),
 	path('logout/',logout_v,name='logout_url'),
     path('',home,name='home-url')
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -17,6 +17,41 @@ def home(request):
 	else:
 		return redirect('/login/')
 
+def marketing_hub(request):
+	# print(request.user.username)
+	if request.user.is_authenticated:
+		user = request.user.username
+		context = {
+			'username': user
+		}
+		return render(request,'marketing.html',context)
+	else:
+		return redirect('/login/')
+
+
+def sales_hub(request):
+	# print(request.user.username)
+	if request.user.is_authenticated:
+		user = request.user.username
+		context = {
+			'username': user
+		}
+		return render(request,'sales.html',context)
+	else:
+		return redirect('/login/')
+
+
+
+def customer_support(request):
+	# print(request.user.username)
+	if request.user.is_authenticated:
+		user = request.user.username
+		context = {
+			'username': user
+		}
+		return render(request,'customer.html',context)
+	else:
+		return redirect('/login/')
 
 
 
